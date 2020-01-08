2b0884e0534074f7f577ad07c46d6d2085881f0c
tyrng <ongtypc@gmail.com>
2020-01-08 22:22:38 +0800

    add accuracy and confusion matrix

--- moreEval/accuracyAndConfusionMatrix.py
+++ moreEval/accuracyAndConfusionMatrix.py
@@ -8,6 +8,10 @@ tf.enable_eager_execution()
 import numpy as np
 from PIL import Image
 import cv2
+import pandas as pd
+import tkinter
+import matplotlib
+import matplotlib.pyplot as plt
 
 from _io import BytesIO
 
@@ -32,6 +36,14 @@ parser.add_argument("--tfrecord_path", type=str,
                     help="Path to tfrecord")
 parser.add_argument("--skip_every", type=int,
                     help="Path to tfrecord")
+parser.add_argument("--top", type=int,
+                    help="Top n accuracy")
+parser.add_argument("--minimumScore", type=float,
+                    help="(float) for minimum score to be considered as valid detection")
+parser.add_argument("--outputResult", type=str,
+                    help="Required text file for output.")
+parser.add_argument("--confusion_topn", type=bool,
+                    help="Whether confusion matrix uses Top-N results, or just Top-1 results.")
 
 args = parser.parse_args()
 
@@ -52,6 +64,25 @@ PATH_TO_TFRECORD = args.tfrecord_path
 NUM_CLASSES = args.num_classes
 
 SKIP_EVERY = args.skip_every
+
+OUTPUT = args.outputResult
+
+MIN_SCORE = args.minimumScore
+
+TOP = args.top
+
+CONFUSION_TOPN = args.confusion_topn
+
+def plot_confusion_matrix(df_confusion, title='Confusion matrix', cmap=plt.cm.jet):
+    plt.matshow(df_confusion, cmap=cmap) # imshow
+    #plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(df_confusion.columns))
+    plt.xticks(tick_marks, df_confusion.columns, rotation=90)
+    plt.yticks(tick_marks, df_confusion.index)
+    #plt.tight_layout()
+    plt.ylabel(df_confusion.index.name)
+    plt.xlabel(df_confusion.columns.name)
 
 # Load the label map.
 # Label maps map indices to category names, so that when our convolution
@@ -97,13 +128,8 @@ with detection_graph.as_default():
 
     sess = tf.Session(graph=detection_graph)
     # op = sess.graph.get_operations()
-    # orig_stdout = sys.stdout
-    # f = open('/Users/tyrone/Desktop/1/moreEval/tensors.txt', 'w')
-    # sys.stdout = f
     # for m in op:
     #     print(m.values())
-    # sys.stdout = orig_stdout
-    # f.close()
 
 # Define input and output tensors (i.e. data) for the object detection classifier
 
@@ -112,7 +138,7 @@ image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
 
 # Output tensors are the detection boxes, scores, and classes
 # Each box represents a part of the image where a particular object was detected
-detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+#detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 
 # Each score represents level of confidence for each of the objects.
 # The score is shown on the result image, together with the class label.
@@ -120,62 +146,98 @@ detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
 detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 
 # Number of objects detected
-num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+#num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
+
+print('Please wait...')
+
+orig_stdout = sys.stdout
+
+y_actu = pd.Series([], name='Actual')
+# y_actu.set_value(0,'None')
+y_pred = pd.Series([], name='Predicted')
+# y_pred.set_value(0,'None')
 
 n = 0
 i = 0
+j = 0
+correct = []
+for g in range(0,TOP):
+  correct.append(0)
 for image_features in parsed_image_dataset:
   if n % SKIP_EVERY == 0:
-    i = i + 1
-    #image_raw = image_features['image/encoded'].numpy()
-    #f = open("/Users/tyrone/Desktop/1/moreEval/output/" + str(i) + '-eval.png', 'wb')
-    #f.write(image_raw)
-    #f.close()
+    trigger = []
+    for g in range(0,TOP):
+      trigger.append(False)
+    f = open(OUTPUT, 'a')
+    sys.stdout = f
     image_reader = tf.image.decode_jpeg(image_features['image/encoded'])
     float_caster = tf.cast(image_reader, tf.float32)
     image_expanded = np.expand_dims(float_caster, axis=0)
     (scores, classes) = sess.run([detection_scores, detection_classes],feed_dict={image_tensor: image_expanded})
-    print(str(i) + ' --- ', end=" ")
+    print(i+1, 'at', n+1, end=" --- ")
+    if CONFUSION_TOPN == False:
+      if scores[0][0] >= MIN_SCORE:
+        # y_actu.set_value(n,int(image_features['image/object/class/label'].values[0].numpy()))
+        y_actu.set_value(i,str(image_features['image/object/class/text'].values[0].numpy()).split("'")[1])
+        # y_pred.set_value(n,int(category_index[classes[0][x].astype(np.int32)]['id']))
+        y_pred.set_value(i,str(category_index[classes[0][0].astype(np.int32)]['name']))
+      else:
+        y_pred.set_value(i,'None')
     print(str(image_features['image/object/class/text'].values[0].numpy()).split("'")[1])
-    for x in range(0,5):
-        print('{}%'.format(int(100*scores[0][x])), end=" ")
+    for x in range(0,TOP):
+      print('{0:.2f}%'.format(round(100*scores[0][x],2)), end=" ")
+      if scores[0][x] > 0.0:
         print(category_index[classes[0][x].astype(np.int32)]['name'])
-    # print ('scores: ')
-    # for s in scores:
-    #     q = 0
-    #     for ss in s:
-    #         if q < 3:
-    #             print(ss)
-    #         q = q + 1
-    # print ('classes: ')
-    # for cc in classes:
-    #     q = 0
-    #     for ccc in cc:
-    #         if q < 3:
-    #             print(category_index[ccc.astype(np.int32)])
-    #         q = q + 1
-    # print ('num: ')
-    # for n in num:
-    #     print(n.astype(np.int32))
-    # Draw the results of the detection (aka 'visulaize the results')
-    # vis_util.visualize_boxes_and_labels_on_image_array(
-    #     float_caster,
-    #     np.squeeze(boxes),
-    #     np.squeeze(classes).astype(np.int32),
-    #     np.squeeze(scores),
-    #     category_index,
-    #     use_normalized_coordinates=True,
-    #     line_thickness=8,
-    #     min_score_thresh=0.60)
+      else:
+        print('None')
 
-    # All the results have been drawn on image. Now display the image.
-    #cv2.imshow('Object detector', float_caster)
+      if CONFUSION_TOPN == True:
+        if scores[0][x] >= MIN_SCORE:
+          # y_actu.set_value(n,int(image_features['image/object/class/label'].values[0].numpy()))
+          y_actu.set_value(j,str(image_features['image/object/class/text'].values[0].numpy()).split("'")[1])
+          # y_pred.set_value(n,int(category_index[classes[0][x].astype(np.int32)]['id']))
+          y_pred.set_value(j,str(category_index[classes[0][x].astype(np.int32)]['name']))
+        # else:
+        #   # y_pred.set_value(j+1,'None')
 
-    # Press any key to close the image
-    #cv2.waitKey(0)
+      pred = int(category_index[classes[0][x].astype(np.int32)]['id'])
+      act = int(image_features['image/object/class/label'].values[0].numpy())
+      if x > 0 and trigger[x-1] == True:
+        trigger[x] = True
+      if pred == act and scores[0][x] >= MIN_SCORE:
+        trigger[x] = True
+      if trigger[x] == True:
+        correct[x] = correct[x] + 1
+      # print(trigger)
+      j = j + 1
+    # print(correct)
 
-    # Clean up
-    #cv2.destroyAllWindows()
-
+    sys.stdout = orig_stdout
+    f.close()
+    i = i + 1
   n = n + 1
+
+
+f = open(OUTPUT, 'a')
+sys.stdout = f
+accuracy = []
+for x in range(0,TOP):
+  accuracy.append(float(correct[x]) / i)
+  print('TOP', x+1, 'ACCURACY =', '{0:.2f}%'.format(round(100*accuracy[x],2)))
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000000)
+
+df_confusion = pd.crosstab(y_actu, y_pred, rownames=['Actual'], colnames=['Predicted'], margins=True)
+df_conf_norm = df_confusion / df_confusion.sum(axis=1)
+
+print(df_conf_norm)
+
+sys.stdout = orig_stdout
+f.close()
+
+
+matplotlib.use('TkAgg')
+matplotlib.pyplot.show(plot_confusion_matrix(df_conf_norm))
